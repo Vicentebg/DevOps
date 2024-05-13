9cc9ec2dfead9493f1bd9967c2d17c79e53717ad
Vicentebg <vicentinhobg@hotmail.com>
2024-05-13 16:44:30 -0300

    update

--- Python/Parte 9 - Dominando Funcoes Python/funcoes_parte_1.py
+++ Python/Parte 9 - Dominando Funcoes Python/funcoes_parte_1.py
@@ -50,3 +50,39 @@ salvar_carro("Fiat", "Palio", 1999, "ABC-1234")
 salvar_carro(marca="Fiat", modelo="Palio", ano=1999, placa="ABC-1234")
 salvar_carro(**{"marca": "Fiat", "modelo": "Palio", "ano": 1999, "placa": "ABC-1234"})
 
+# Args e kwargs
+# Podemos combinar parâmetros obrigatórios com args e kwargs. 
+# Quando essessão definidos (*args[são renderizados como uma tupla] e **kwargs[são renderizados como dicionário]), o método recebe os valores como tupla e dicionário respectivamente.
+
+def exibir_poema(data_extenso, *args, **kwargs):
+    texto = "\n".join(args)
+    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
+    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
+    print(mensagem)
+
+
+exibir_poema(
+    "Sexta-feira, 26 de Agosto de 2022",
+    "Zen of Python",
+    "Beautiful is better than ugly.",
+    "Explicit is better than implicit.",
+    "Simple is better than complex.",
+    "Complex is better than complicated.",
+    "Flat is better than nested.",
+    "Sparse is better than dense.",
+    "Readability counts.",
+    "Special cases aren't special enough to break the rules.",
+    "Although practicality beats purity.",
+    "Errors should never pass silently.",
+    "Unless explicitly silenced.",
+    "In the face of ambiguity, refuse the temptation to guess.",
+    "There should be one-- and preferably only one --obvious way to do it.",
+    "Although that way may not be obvious at first unless you're Dutch.",
+    "Now is better than never.",
+    "Although never is often better than *right* now.",
+    "If the implementation is hard to explain, it's a bad idea.",
+    "If the implementation is easy to explain, it may be a good idea.",
+    "Namespaces are one honking great idea -- let's do more of those!",
+    autor="Tim Peters",
+    ano=1999,
+)
---
+++ Python/Parte 9 - Dominando Funcoes Python/funcoes_parte_2.py
@@ -0,0 +1,3 @@
+# Parâmetros especiais
+# Por padrão, argumentos podem ser passados para uma função Python tanto por posição quanto explicitamente pelo nome.
+# Para uma melhor legibilidade e desempenho, faz sentido restringir a maneira pelo qual argumentos possam ser passados, assim um desenvolvedor precisa apenas olhar para a definição da função para determinar se os itens são passados por posição, por posição e nome, ou por nome.

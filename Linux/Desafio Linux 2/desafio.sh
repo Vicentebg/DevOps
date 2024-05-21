b1d40f8e64713b2ec7a750f5114bf73bb8bf04d3
Vicentebg <vicentinhobg@hotmail.com>
2024-05-21 16:34:45 -0300

    Desafio

---
+++ Linux/Desafio Linux 2/desafio.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+### Iniciando o Script ###
+
+echo "Iniciando o Script"
+
+### Atualizando Servidor ###
+
+echo "Atualizando Servidor"
+apt-get update
+apt-get upgrade -y
+
+### Instalando Apache ###
+
+echo "Instalando Apache"
+apt-get install apache2 -y
+
+### Instalando Unzip ###
+
+echo "Instalando Unzip"
+apt-get install unzip -y
+
+### Baixando aplicação ###
+
+echo "Baixando aplicação"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio
+
+### Copiando arquivos ###
+
+echo "Copiando arquivos"
+cp -R * /var/www/html/
---
+++ Linux/Desafio Linux 2/readme.md
@@ -0,0 +1,15 @@
+# Definições
+
+- Restaurar o snapshot criado anteriormente no virtualbox;
+- Atualizar o servidor;
+- Instalar o apache2;
+- Instalar o unzip;
+- Baixar a aplicação disponível no endereço https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip no diretório /tmp;
+- Copiar os arquivos da aplicação no diretório padrão do apache;
+- Subir arquivo de script para um repositório no GitHub.
+
+
+Para executar o script devemos dar a permissão `chmod +x desafio.sh` e logo em seguida `./desafio.sh`
+
+
+
